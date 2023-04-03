9e44e0f78afbd5a91964020493ba33752ac2eb82
sam <slerman@u.rochester.edu>
2023-04-03 14:44:20 -0400

    launcher

--- Remote/sbatch.py
+++ Remote/sbatch.py
@@ -91,13 +91,13 @@ python3 {path}/{run} {" ".join([f"'{key}={getattr_recursive(args, key.strip('+')
 """
 
     # Write script
-    with open(path + '/sbatch_script', 'w') as file:
+    with open('./sbatch_script', 'w') as file:
         file.write(script)
 
     # Launch script (with error checking / re-launching)
     while True:
         try:
-            success = str(subprocess.check_output([f'sbatch {path}/sbatch_script'], shell=True))
+            success = str(subprocess.check_output([f'sbatch ./sbatch_script'], shell=True))
             print(success[2:][:-3])
             if "error" not in success:
                 break
