b253df740b45679ebb40db4810770616fba1ebf1
sam <slerman@u.rochester.edu>
2023-04-20 18:55:14 -0400

    Debugging launcher for apps

--- Sweeps/XRD/Paper1.py
+++ Sweeps/XRD/Paper1.py
@@ -8,13 +8,13 @@ runs.sweep = [
     # Large + RRUFF, No-Pool-CNN
     """task=npcnn
     task_name='${num_classes}-Way_ICSD-true_Open-Access-false_RRUFF-true_Soup-true'
-    num_classes=7,230
+    num_classes=230
     train_steps=5e5
     +'dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd1.2m_large/","/scratch/slerman/XRDs_backup/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
     +'dataset.train_eval_splits=[1, 0.5]'
     parallel=true
     num_gpus=4
-    mem=80
+    mem=180
     reservation_id=20230321""",
 ]
 
