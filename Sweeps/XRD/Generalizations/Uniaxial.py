663e1b4a5c2ff39c8d0aafa39dfbe423edf36868
sam <slerman@u.rochester.edu>
2023-04-23 16:49:26 -0400

    XRD Uniaxial experiments

--- Sweeps/XRD/Generalizations/Uniaxial.py
+++ Sweeps/XRD/Generalizations/Uniaxial.py
@@ -6,18 +6,18 @@ runs = template()
 runs.sweep = [
     # Large + RRUFF, No-Pool-CNN
     f"""task=npcnn
+    train_steps=0
     experiment={experiment}
-    num_classes=230
-    task_name='${{num_classes}}-Way_ICSD-true_Open-Access-false_RRUFF-true_Soup-true'
+    num_classes=7
     load=true
     load_path='/scratch/slerman/XRDs/Checkpoints/NPCNN/AC2Agent/classify/${{task_name}}_1.pt'
-    train_steps=0
+    task_name='${{num_classes}}-Way_ICSD-true_Open-Access-false_RRUFF-true_Soup-true'
     +'dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/icsd1.2m_large/","/scratch/slerman/XRDs_backup/icsd_Datasets/rruff/XY_DIF_noiseAll/"]'
     +'dataset.train_eval_splits=[1, 0.5]'
     TestDataset=XRD.XRD
     +'test_dataset.roots=["/gpfs/fs2/scratch/public/jsalgad2/Uniaxial/{experiment}/"]'
     +'test_dataset.train_eval_splits=[0]'
-    +test_dataset.num_classes=${{num_classes}}
+    +'test_dataset.num_classes=${{num_classes}}'
     mem=80
     reservation_id=20230321""" for experiment in ['090', '095', '099']
 ]
