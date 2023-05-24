da56453f72bb86a9efa0aca959d455bd4f4cd7c2
sam <slerman@u.rochester.edu>
2023-05-24 15:08:46 -0400

    Tributaries

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) AGI.__init__. All Rights Reserved.
-#
-# This source code is licensed under the MIT license found in the
-# MIT_LICENSE file in the root directory of this source tree.
