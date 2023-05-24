043dc1a92fd801fb23cdd7d69b3a5182d4e53983
sam <slerman@u.rochester.edu>
2023-05-24 12:09:57 -0400

    Launch

--- Remote/sbatch.py
+++ Remote/sbatch.py
@@ -94,7 +94,7 @@ def main(args):
 {extra}
 {sbatch if sbatch else ''}
 {conda if conda else ''}
-{"wandb login " + args.wandb_key if args.wandb_key else ''}'
+{"wandb login " + args.wandb_key if args.wandb_key else ''}
 python {path}/{run} {" ".join([f"'{key}={getattr_recursive(args, key.strip('+'))}'" for key in sys_args - meta])}
 """
 
